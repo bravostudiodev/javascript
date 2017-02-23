731bd5c7e0aedf50018454ad1b48b753fe366a16
vladimir <vladimir@propelmedia.com>
2017-02-23 11:12:05 +0100

    [ASD-859] Removed no-use-before-define rule, it doesn't make sense since we're shuffling code.

--- rules/variables.js
+++ rules/variables.js
@@ -34,6 +34,6 @@ module.exports = {
     'no-unused-vars': [2, { vars: 'local', args: 'after-used' }],
 
     // disallow use of variables before they are defined
-    'no-use-before-define': 2
+    'no-use-before-define': 0
   }
 };
