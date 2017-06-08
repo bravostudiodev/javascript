375d0d487f731e4672753ad824d6aeacc0e913f9
Vladimir Cutkovic <vladimir@propelmedia.com>
2017-06-08 15:25:39 +0200

    Changed rule for unnecessary quotes in JS objects from as-needed to consistent-as-needed

--- rules/style.js
+++ rules/style.js
@@ -234,7 +234,7 @@ module.exports = {
 
     // require quotes around object literal property names
     // http://eslint.org/docs/rules/quote-props.html
-    'quote-props': [2, 'as-needed', { keywords: false, unnecessary: true, numbers: false }],
+    'quote-props': [2, 'consistent-as-needed', { keywords: false, unnecessary: true, numbers: false }],
 
     // specify whether double or single quotes should be used
     quotes: [2, 'single', { avoidEscape: true }],
